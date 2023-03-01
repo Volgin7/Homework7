94adbd2ffcf7ae022a2f377feef403cfb372fcfa
Volgin7 <8612020@gmail.com>
2023-03-01 12:59:53 +0300

    +task2

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         task1();
+        task2();
     }
     public static void task1() {
         System.out.println("Задача 1");
@@ -14,8 +15,20 @@ public class Main {
             monthCount++;
         }
         System.out.println("Месяц " + monthCount + ", сумма накоплений равна " + deposit + " рублей");
-
     } // End of task
 
+    public static void task2() {
+        System.out.println("Задача 2");
+        int i = 1;
+        while ( i <= 10) {
+            System.out.print(i + " ");
+            i++;
+        }
+        System.out.println();
+
+        for (i = 10; i  >= 1; i--) {
+            System.out.print(i + " ");
+        }
+    } // End of task
 
 } // End of main
