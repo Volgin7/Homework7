0ea93f52f895436b6bde3d94afa995bd0af824dd
Volgin7 <8612020@gmail.com>
2023-03-01 13:50:02 +0300

    +task4

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,7 @@ public class Main {
         task1();
         task2();
         task3();
+        task4();
     }
     public static void task1() {
         System.out.println("Задача 1");
@@ -29,6 +30,7 @@ public class Main {
         for (i = 10; i >= 1; i--) {
             System.out.print(i + " ");
         }
+        System.out.println();
     } // End of task
 
     public static void task3() {
@@ -37,11 +39,23 @@ public class Main {
         int birthRate = 17;
         int deathRate = 8;
         int growthRate = birthRate - deathRate;
-
         for ( int i=1; i <= 10; i++) {
             population += population * growthRate / 1_000;
             System.out.println("Год " + i + ", численность населения составляет " + population);
         }
     } // End of task
+
+    public static void task4() {
+        System.out.println("Задача 4");
+        float deposit =  15_000f;
+        float interestRate = 0.07f;
+        float targetDeposit = 12_000_000f;
+
+        for ( int i=1; deposit < targetDeposit; i++) {
+            deposit *= 1 + interestRate;
+            System.out.println("Месяц = " + i + ", сумма вклада = " + deposit + " рублей");
+        }
+    } // End of task
+
 
 } // End of main
