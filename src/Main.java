f7c4c92dc42e2c04111e53ce1260d6c688395f04
Volgin7 <8612020@gmail.com>
2023-03-01 12:48:54 +0300

    +task1

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,21 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        task1();
     }
+    public static void task1() {
+        System.out.println("Задача 1");
+        int deposit = 0;
+        int monthlyDeposit = 15_000;
+        int targetDeposit = 2_459_000;
+        int monthCount = 0;
+
+        while ( deposit < targetDeposit) {
+            deposit += monthlyDeposit;
+            monthCount++;
+        }
+        System.out.println("Месяц " + monthCount + ", сумма накоплений равна " + deposit + " рублей");
+
+    } // End of task
+
 
 } // End of main
